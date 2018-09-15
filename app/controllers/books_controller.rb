5c02751e17c0d8eded89e17125b4855db30d4e62
Janusz Kojro <januszkojro@MacBook-Pro-Janusz.local>
2018-09-15 16:17:29 +0200

    Add books controller's methods and tests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,46 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :destroy, :edit, :update]
   def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def edit
+  end
+
+  def update
+    @book.update(book_params)
+
+    redirect_to @book
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :description)
   end
 end
---
+++ spec/controllers/book_controller_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe BooksController, typr: :controller do
+  describe 'GET #index' do
+    it 'assigns @books' do
+      book = Book.create(title:'test title', description: 'test description')
+      get :index
+      expect(assigns(:books)).to eq([book])
+    end
+
+    it 'returns http success' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      book = Book.create(title:'title', description: 'description')
+      get :show, params: {id: book}
+      expect(response).to render_template('show')
+    end
+  end
+
+  describe 'GET #create' do
+    it 'creates book' do
+      expect do
+        post :create, params: { book: {title:'title', description: 'desc'} }
+      end.to change(Book, :count).by(1)
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it 'renders :new when invalid data' do
+      expect do
+        post :create, params: { book: {title:'', description: 'desc'} }
+      end.to_not change(Book, :count)
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'edit book' do
+      book = Book.create(title:'edit test title', description: 'edit test description')
+      get :edit, params: {id: book}
+      expect(response).to be_successful
+      expect(assigns(:book)).to eq(book)
+    end
+  end
+
+  describe 'PUT #update' do
+    it 'updates book title and description' do
+      book = Book.create(title:'title', description: 'description')
+      # new_params = (title: 'updated title', description: 'updated desc')
+      patch :update, params: { id: book.id, book: {title: 'updated title', description: 'updated desc'}}
+      expect(book.reload.title).to eq 'updated title'
+      expect(book.reload.description).to eq 'updated desc'
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'deleted book' do
+      book = Book.create(title:'title', description: 'description')
+
+      expect do
+        delete :destroy, params: {id: book}
+      end.to change(Book, :count).by(-1)
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+end
